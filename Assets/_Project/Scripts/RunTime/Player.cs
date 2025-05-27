6e88c8e4c42a7b482ddb34fbbb11cc530e9ec69f
Fedir05 <150347261+Fedir05@users.noreply.github.com>
2025-05-27 22:26:18 +0200

    add new class OnCollisionEnter and MovePlayer

--- Assets/_Project/Scripts/RunTime/Player.cs
+++ Assets/_Project/Scripts/RunTime/Player.cs
@@ -1,16 +1,41 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Rigidbody rigidbody;
+
+    [SerializeField] private float moveSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
-
+    
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        MovePlayer();
+    }
+
+    public void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(other.gameObject.name);
+    }
+
+    public void MovePlayer()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidbody.AddForce(-1f * moveSpeed, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rigidbody.AddForce(Vector3.right * moveSpeed);
+        }
+        else
+        {
+            rigidbody.linearVelocity = Vector3.zero;
+        }
     }
 }
