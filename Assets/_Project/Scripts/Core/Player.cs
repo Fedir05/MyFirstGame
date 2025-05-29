011753ff3c198f646e543d77e6e1c3854d5c641c
Fedir05 <150347261+Fedir05@users.noreply.github.com>
2025-05-29 22:54:56 +0200

    add new method Player and new property

---
+++ Assets/_Project/Scripts/Core/Player.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Player : Person
+{
+    [SerializeField] private int experience;
+
+    public int Experience
+    {
+        get{return experience;}
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
